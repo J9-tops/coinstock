35b0b21543a580ceddb139810ab474f59cd6368d
J9-tops <j9.tops@gmail.com>
2025-10-11 21:18:31 +0100

    style: adjust style for overview section

---
+++ src/app/components/dashboard/overview/overview.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'overview',
+  imports: [],
+  templateUrl: './overview.html',
+  styleUrl: './overview.css',
+})
+export class Overview {}
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,3 +1,4 @@
+import { Overview } from '@/app/components/dashboard/overview/overview';
 import { Header } from '@/app/components/shared/header/header';
 import { Navbar } from '@/app/components/shared/navbar/navbar';
 import { Component } from '@angular/core';
@@ -5,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, Header, Navbar],
+  imports: [RouterOutlet, Header, Navbar, Overview],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
