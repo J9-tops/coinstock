2e0f712a0fbccb48f9282899979b182655629dff
J9-tops <j9.tops@gmail.com>
2025-10-13 12:20:53 +0100

    feat: complete table

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,12 @@
 import { Overview } from '@/app/components/dashboard/overview/overview';
+import { Table } from '@/app/components/dashboard/table/table';
 import { Header } from '@/app/components/shared/header/header';
 import { Navbar } from '@/app/components/shared/navbar/navbar';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Header, Navbar, Overview],
+  imports: [Header, Navbar, Overview, Table],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
