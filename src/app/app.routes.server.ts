2528e17199268199fab0d86f9dce45315d7af481
J9-tops <j9.tops@gmail.com>
2025-10-19 16:30:27 +0100

    fix: fix route error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: 'dashboard',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'dashboard/:page',
+    renderMode: RenderMode.Server,
+  },
 ];
