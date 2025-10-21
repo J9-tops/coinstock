2d1e650f39b27bd5e374728c8b0015a2a873e380
J9-tops <j9.tops@gmail.com>
2025-10-21 12:26:55 +0100

    fix: fix rendering issue

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'dashboard/:page',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'dashboard',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Prerender,
   },
 ];
