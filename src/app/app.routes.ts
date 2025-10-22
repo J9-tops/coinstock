4577a2619f8e662e7e707a72f07e12e5327a5764
J9-tops <j9.tops@gmail.com>
2025-10-22 13:46:41 +0100

    feat: add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './components/home-page/home-page';
 import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
+    component: HomePage,
   },
   {
     path: 'dashboard',
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,4 @@ import { Router, RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('trade-scope');
-
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    this.router.navigate(['/dashboard']);
-  }
 }
